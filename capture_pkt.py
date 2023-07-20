761cd1763d1b62a9f15deb406de8b5db9e872087
lwh <809578767@qq.com>
2023-07-20 19:02:08 +0800

    test

--- capture_pkt.py
+++ capture_pkt.py
@@ -16,7 +16,7 @@ filter_ip = "2001:250:1001:1044::9d"
 def capture_pkt():
     global pcap_counter
     while capture_flag:
-        # TODO 这里是用IP区分当前视频服务的包和其他包，在真实场景中，该如何获取本机ip或视频服务器对应的ip需进一步改进
+        # TODO 这里是用IP区分当前视频服务的包和其他包，在真实场景中，该如何获取本机ip或视频服务器对应的ip需进一步改进 #
         print('开始捕获网络包')
         host = "2001:250:1001:1044::9d"
         filter_str = f"ip6 host {host}"
@@ -30,5 +30,6 @@ def capture_pkt():
         save_thread.start()
         pcap_queue.put(pcap_filename)
         write_event.set()
+        
 
         pcap_counter += 1
